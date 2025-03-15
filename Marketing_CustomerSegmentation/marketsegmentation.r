f83173b788c93f1f76c4d2ca72e3f2b01ae14653
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2025-03-15 23:33:25 +0700

    Project Initiation

---
+++ Marketing_CustomerSegmentation/marketsegmentation.r
@@ -0,0 +1,4 @@
+# Assigning Datasets
+# pelanggan <- read.csv("https://storage.googleapis.com/dqlab-dataset/customer_segments.txt", sep="\t")
+pelanggan <- read.csv("customer_segments.txt", sep="\t")
+pelanggan[c("Jenis.Kelamin","Umur","Profesi","Tipe.Residen")]
