7cb251a86249a5e1af96929f7e89e4dc679cd877
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2025-03-17 11:34:25 +0700

    Item Set Combination Analysis

--- Retail_MarketBasketAnalysis/productcombination_small.r
+++ Retail_MarketBasketAnalysis/productcombination_small.r
@@ -5,11 +5,5 @@ library(arules)
 # transaksi <- read.transactions(file="https://storage.googleapis.com/dqlab-dataset/data_transaksi.txt", format="single", sep="\t", cols=c(1,2), skip=1)
 transaksi <- read.transactions(file="Retail_MarketBasketAnalysis/data_transaksi.txt", format="single", sep="\t", cols=c(1,2), skip=1)
 
-#Menampilkan data transaksi dengan print dan inspect 
-inspect(transaksi)
-
-#Menghasilkan model Market Basket Analysis
-mba <- apriori(transaksi,parameter = list(supp = 0.1, confidence = 0.5))
-
-#Menampilkan paket produk
-inspect(subset(mba, lift>1))
+#Menampilkan jumlah kombinasi dari produk yang terdapat pada daftar transaksi yang ada
+inspect(apriori(transaksi, parameter = list(support=.1, minlen=2, target='frequent itemsets')))
